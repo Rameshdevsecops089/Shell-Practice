e8e1b7c807bba6a9f457c8f53c9187df9a836972
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-17 00:03:08 +0530

    variables file added

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 echo "trump :: hey musk, How are you?"
 echo "musk :: Hello trump, i am fine. how are you do"
-echo "trums: i am fine too. whats up?"
+echo "trump: i am fine too. whats up?"
 echo "musk : nothing, just going to marri now, will you com"
-
+echo "trump : sorry, you carry on! i will come "
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=trump
+PERSON2=musk
+
+echo "$person1 :: hey $person2, How are you?"
+echo "$person2 :: Hello $person1, i am fine. how are you do"
+echo "$person1: i am fine too. whats up?"
+echo "$person2 : nothing, just going to marri now, will you com"
+echo "$person1 : sorry, you carry on! i will come "
