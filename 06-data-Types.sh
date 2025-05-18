4896fef989b306e9e6fe89738b583b0fe479d87b
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-18 12:17:30 +0530

    data types added

--- 06-data-Types.sh
+++ 06-data-Types.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!bin/bash
 
-Timestamp=$(date)
+Timestamp = $(date)
 echo "script execute at: $Timestamp"
+
+number1=100
+number2=200
+
+sum=$number1 + $number2
+
+echo "sum of $number1 and $number2 is:: $sum"
+
