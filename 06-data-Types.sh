6cc96fed240dbe72c88f4350ff2dd70653d64a72
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-18 12:29:56 +0530

    data commited subiscrtion

--- 06-data-Types.sh
+++ 06-data-Types.sh
@@ -7,6 +7,8 @@ number1=100
 number2=200
 
 sum=$(($number1+$number2))
-
+sub=$(($number1*$number2))
 echo "sum of $number1 and $number2 is:: $sum"
+
+echo "substrction of $number1 and $number2 is :: $sub"
 
