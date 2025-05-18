9e4e9a2d2e3d5e649dad97ef61bc2dec4734b73d
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-18 12:42:16 +0530

    data array added

---
+++ 07-array.sh
@@ -0,0 +1,7 @@
+movies = ("court" "Hit" "Pushpa1" "killer")
+
+echo "first movie:: ${movie[0]}"
+echo "third movie:: ${movie[2]}"
+echo "forth movie:: ${movie[4]}"
+
+echo "All movies:: ${movies[@]}"
