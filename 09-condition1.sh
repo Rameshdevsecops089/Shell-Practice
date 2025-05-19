750e450f396798f202f8ac93b484d6d8f35f44c0
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-19 16:20:44 +0530

    condition added

--- 09-condition1.sh
+++ 09-condition1.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -lt 20]
+if [ $NUMBER -lt 10 ]
 then 
-    echo "given number $NUMBER is less than 20"
+    echo "given number $NUMBER is less than 10"
 else
-    echo "given number $NUMBER is not less than 20"
+    echo "given number $NUMBER is not less than 10"
 fi
