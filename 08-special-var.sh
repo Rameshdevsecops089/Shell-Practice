6b087a7bdf0abf88661b3b9ebdddf470f1837210
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-19 15:34:04 +0530

    special data added

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -3,3 +3,10 @@
 echo "Hello from ramesh"
 echo "All variables passed to the script: $@"
 echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
+echo "PID of last command in background: $!"
+
