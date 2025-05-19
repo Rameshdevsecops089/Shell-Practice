baa1095018db7951de1e26b9598b109c9826b5e8
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-19 17:32:42 +0530

    condition added

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,8 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Error:: please run this script with root user"
+    exit 1 #give other than 0 upto 127
 else
     echo "you are running with root user"
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then 
+    echo "install mysql is ... success"
+else
+    echo "install mysql is ... Failure"
+    #exit 1
+fi
