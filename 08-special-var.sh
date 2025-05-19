b0659bb4a96e5a6a49bd832e89450253843c4ceb
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-19 14:26:59 +0530

    special-var added

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-echo "Number of variables: $#"
-echo "Script name: $0"
-echo "Current Directory: $PWD"
-echo "user running this script: $user"
-echo "Home directory of user: $Home"
-echo "PID of the script: $$"
-sleep 10 &
-echo "PID of last command in background: $!"
+
