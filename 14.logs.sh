7ae76659d42abcd6eee1e16b8e6dbb585771712e
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-21 16:28:19 +0530

    colors file added

--- 14.logs.sh
+++ 14.logs.sh
@@ -35,7 +35,7 @@ dnf list install mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "mysql is not installed... going to install it" | tee -a $LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -Y &>>$LOG_FILE
     VALIDATE $1 "mysql"
 else
     echo -e "nothing to do mysql...$Y already installed it $N" | tee -a $LOG_FILE
@@ -45,7 +45,7 @@ dnf list install python3 &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "python3 is not installed... going to install" | tee -a $LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
+    dnf install python3 -Y &>>$LOG_FILE
     VALIDATE $? "python3"
 else
     echo "Nothing to do python3... $Y already installed $N" | tee -a $LOG_FILE
