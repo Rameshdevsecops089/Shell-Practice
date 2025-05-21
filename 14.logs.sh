c32d8b53f981bc1b6474b30f0e259c0ca0481903
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-05-21 15:42:16 +0530

    colors file added

--- 14.logs.sh
+++ 14.logs.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-Logs_FOLDER = "/var/log/shellscript-logs"
+LOGS_FOLDER = "/var/log/shellscript-logs"
 SCRIPT_NAME= $(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
